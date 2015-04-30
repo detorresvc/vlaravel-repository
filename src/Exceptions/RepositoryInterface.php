fcbd4ce16d794e9376d7acd92d542023419f637b
Vincent De Torres <detorresvc@ymail.com>
2015-04-30 13:07:07 +0800

    repo pattern

---
+++ src/Exceptions/RepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php namespace Vince\Repositories\Exception;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of RepositoryInterface
+ *
+ * @author vcdetorres
+ */
+class RepositoryInterface extends Exception {
+    //put your code here
+}
